fbc88108d915a60c908e08395c21f63e0db56c45
Eric Oulashin <eric.oulashin@gmail.com>
2026-03-18 16:56:14 -0700

    Added another 'override' to cxInput.h

--- src/cxInput.h
+++ src/cxInput.h
@@ -280,7 +280,7 @@ class cxInput : public cxWindow
        *
        * @param pExitOnMouseOutside Whether to exit on outside mouse clicks
        */
-      void setExitOnMouseOutside(bool pExitOnMouseOutside);
+      void setExitOnMouseOutside(bool pExitOnMouseOutside) override;
 
       /**
        * \brief Returns whether the input exits when the left arrow key is
