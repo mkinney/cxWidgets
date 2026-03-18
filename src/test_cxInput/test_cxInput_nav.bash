95752550d72a9334a9b33e75f04ea8239e3f73a2
Mike Kinney <mike.kinney@gmail.com>
2026-03-18 13:26:50 -0700

    fail if any compile warnings in ci; fail if any tests fail in test_cxInput

--- src/test_cxInput/test_cxInput_nav.bash
+++ src/test_cxInput/test_cxInput_nav.bash
@@ -25,34 +25,34 @@ run_test() {
 
 # if the value of a cxInput is "set", then ensure
 # that we get that value back out
-run_test 1 "A:" "a" "a"
+run_test 1 "A:" "a" "a" || exit 1
 
 # if the value of a cxInput is "set", then ensure
 # that we get that value back out (changed from test #1)
-run_test 2 "A:" "b" "b"
+run_test 2 "A:" "b" "b" || exit 1
 
 # test that if you set it with a blank that it truly is
 # a blank
-run_test 3 "A:" "" ""
+run_test 3 "A:" "" "" || exit 1
 
 # test what happens if you enter more characters than
 # what's in the field
-run_test 4 "A:" "1234" "124"
+run_test 4 "A:" "1234" "124" || exit 1
 
 # test the HOME key
-run_test 5 "A:" "#HOME#ABC" "ABC"
+run_test 5 "A:" "#HOME#ABC" "ABC" || exit 1
 
 # test the HOME key (as first char)
-run_test 6 "A:" "A#HOME#BC" "BC" "should be:BC: from:A#HOME#BC:"
+run_test 6 "A:" "A#HOME#BC" "BC" "should be:BC: from:A#HOME#BC:" || exit 1
 
 # test the HOME key (as a sequence in the middle)
-run_test 7 "A:" "111#HOME#ABC" "ABC" "should be:ABC: from:111#HOME#ABC:"
+run_test 7 "A:" "111#HOME#ABC" "ABC" "should be:ABC: from:111#HOME#ABC:" || exit 1
 
 # test the BACKSPACE key 
-run_test 8 "A:" "ABC#BACK#D" "ABD" "should be:ABD: from:ABC#BACK#D:"
+run_test 8 "A:" "ABC#BACK#D" "ABD" "should be:ABD: from:ABC#BACK#D:" || exit 1
 
 # test the RIGHT key 
-run_test 9 "A:" "ABC#HOME##RIGHT##RIGHT#D" "ABD" "should be:ABD: from:ABC#HOME##RIGHT##RIGHT#D:"
+run_test 9 "A:" "ABC#HOME##RIGHT##RIGHT#D" "ABD" "should be:ABD: from:ABC#HOME##RIGHT##RIGHT#D:" || exit 1
 
 # test the BACKSPACE key 
-run_test 10 "A:" "ABC#LEFT##BACK#" "AC" "should be:AC: from:ABC#LEFT##BACK#:"
+run_test 10 "A:" "ABC#LEFT##BACK#" "AC" "should be:AC: from:ABC#LEFT##BACK#:" || exit 1
